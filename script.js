edd17c05aa8ad89aaec2c5a551e7c6f8d8390a3d
Teste555555 <teste5555555e@gmail.com>
2024-05-08 20:21:56 -0300

    Create script.js
    JAVA SCRIPT

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para ser executada na página inicial
+function funcaoPaginaInicial() {
+  alert("Bem-vindo ao Apostas Online! Faça suas apostas e divirta-se!");
+}
+
+// Evento ao clicar no botão de fazer aposta
+document.getElementById("btnApostar").addEventListener("click", function() {
+  alert("Faça sua aposta agora!");
+});
